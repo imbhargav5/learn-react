c8ce3bbcef561fe0d891ff83b696923b426c38ef
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2020-02-23 12:29:44 +0530

    Slider

--- 3.context/src/components/Slider.js
+++ 3.context/src/components/Slider.js
@@ -5,11 +5,26 @@ function Slider(props) {
     const { images } = props;
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+    function goToNextSlide() {
+        if (currentImageIndex === images.length - 1) {
+            return setCurrentImageIndex(0);
+        }
+        setCurrentImageIndex(Math.min(currentImageIndex + 1, images.length - 1))
+    }
+
+    function goToPrevSlide() {
+        if (currentImageIndex === 0) {
+            return setCurrentImageIndex(images.length - 1);
+        }
+        setCurrentImageIndex(Math.max(0, currentImageIndex - 1))
+    }
+
     const currentImage = images[currentImageIndex]
     return (
         <div>
             <div><img height="300" width="400" src={currentImage} /></div>
-            <div><button>Prev</button><button>Next</button></div>
+            <div><button onClick={goToPrevSlide}>Prev</button>
+                <button onClick={goToNextSlide}>Next</button></div>
         </div>
     );
 }
